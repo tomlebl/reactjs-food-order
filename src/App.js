5723a965c5556963ab5069d61fe06cfdc514a818
tomlebl <tl12@st-andrews.ac.uk>
2021-11-18 22:30:54 +0000

    merged feature 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react'
+
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import CartProvider from './store/CartProvider'
 import Cart from './components/Cart/Cart'
 
+//##
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
 
